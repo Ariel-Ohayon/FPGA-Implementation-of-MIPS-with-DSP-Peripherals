bf4b225dca956c7c6bcd5f8f4cb4f3b08a19a84e
Ariel Ohayon <83784945+Ariel-Ohayon@users.noreply.github.com>
2023-01-01 22:41:23 +0200

    Update Shift_reg.vhd
    Add input sel bits [7 downto 0]

--- Shift_reg.vhd
+++ Shift_reg.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity Shift_reg is
 port(
 	input:	in	std_logic_vector(23 downto 0);
-	sel:	in	std_logic_vector(4  downto 0);
+	sel:	in	std_logic_vector(7  downto 0);
 	output:	out	std_logic_vector(23 downto 0));
 end;
 
@@ -13,30 +13,30 @@ begin
 	process(sel,input)
 	begin
 	case (sel) is
-		when "00000" => output <= input;
-		when "00001" => output <= '0' & input(23 downto 1);
-		when "00010" => output <= "00" & input(23 downto 2);
-		when "00011" => output <= "000" & input(23 downto 3);
-		when "00100" => output <= "0000" & input(23 downto 4);
-		when "00101" => output <= "00000" & input(23 downto 5);
-		when "00110" => output <= "000000" & input(23 downto 6);
-		when "00111" => output <= "0000000" & input(23 downto 7);
-		when "01000" => output <= "00000000" & input(23 downto 8);
-		when "01001" => output <= "000000000" & input(23 downto 9);
-		when "01010" => output <= "0000000000" & input(23 downto 10);
-		when "01011" => output <= "00000000000" & input(23 downto 11);
-		when "01100" => output <= "000000000000" & input(23 downto 12);
-		when "01101" => output <= "0000000000000" & input(23 downto 13);
-		when "01110" => output <= "00000000000000" & input(23 downto 14);
-		when "01111" => output <= "000000000000000" & input(23 downto 15);
-		when "10000" => output <= "0000000000000000" & input(23 downto 16);
-		when "10001" => output <= "00000000000000000" & input(23 downto 17);
-		when "10010" => output <= "000000000000000000" & input(23 downto 18);
-		when "10011" => output <= "0000000000000000000" & input(23 downto 19);
-		when "10100" => output <= "00000000000000000000" & input(23 downto 20);
-		when "10101" => output <= "000000000000000000000" & input(23 downto 21);
-		when "10110" => output <= "0000000000000000000000" & input(23 downto 22);
-		when "10111" => output <= "00000000000000000000000" & input (23);
+		when x"00" => output <= input;
+		when x"01" => output <= '0' & input(23 downto 1);
+		when x"02" => output <= "00" & input(23 downto 2);
+		when x"03" => output <= "000" & input(23 downto 3);
+		when x"04" => output <= "0000" & input(23 downto 4);
+		when x"05" => output <= "00000" & input(23 downto 5);
+		when x"06" => output <= "000000" & input(23 downto 6);
+		when x"07" => output <= "0000000" & input(23 downto 7);
+		when x"08" => output <= "00000000" & input(23 downto 8);
+		when x"09" => output <= "000000000" & input(23 downto 9);
+		when x"0A" => output <= "0000000000" & input(23 downto 10);
+		when x"0B" => output <= "00000000000" & input(23 downto 11);
+		when x"0C" => output <= "000000000000" & input(23 downto 12);
+		when x"0D" => output <= "0000000000000" & input(23 downto 13);
+		when x"0E" => output <= "00000000000000" & input(23 downto 14);
+		when x"0F" => output <= "000000000000000" & input(23 downto 15);
+		when x"10" => output <= "0000000000000000" & input(23 downto 16);
+		when x"11" => output <= "00000000000000000" & input(23 downto 17);
+		when x"12" => output <= "000000000000000000" & input(23 downto 18);
+		when x"13" => output <= "0000000000000000000" & input(23 downto 19);
+		when x"14" => output <= "00000000000000000000" & input(23 downto 20);
+		when x"15" => output <= "000000000000000000000" & input(23 downto 21);
+		when x"16" => output <= "0000000000000000000000" & input(23 downto 22);
+		when x"17" => output <= "00000000000000000000000" & input (23);
 		when others => output <= (others => '0');
 	end case;
 	end process;
