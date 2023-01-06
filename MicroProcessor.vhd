a9e53ce4e3645ccf8e6cbf5648a4ac994c8e7c88
Ariel Ohayon <83784945+Ariel-Ohayon@users.noreply.github.com>
2023-01-06 12:19:49 +0200

    Update MicroProcessor.vhd

--- MicroProcessor.vhd
+++ MicroProcessor.vhd
@@ -1,3 +1,7 @@
+-- Designed By: Ariel Ohayon
+
+-- Final Microprocessor Module (not finished)
+
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -8,7 +12,24 @@ port(
 	Instruction_Data_input:		in	std_logic_vector(7	downto 0);
 	Instruction_Address_input:	in	std_logic_vector(31 downto 0);
 	En_Program:					in	std_logic;	-- En_Program = '1' Read state	|	En_Program = '0' Write State
-	Instruction_Data_output:	out	std_logic_vector(31 downto 0));
+	
+	R_Type:		out std_logic;
+	I_Type:		out std_logic;	
+	J_Type:		out std_logic;
+	BUS_Type:	out std_logic;
+			
+	ReadReg1:	out std_logic_vector(4 downto 0);
+	ReadReg2:	out std_logic_vector(4 downto 0);
+	WriteReg:	out std_logic_vector(4 downto 0);
+	
+	Op_Code:	out std_logic_vector(1 downto 0);
+	Op_Shift:	out std_logic;
+	Op_Arith:	out std_logic;
+	Op_Logic:	out std_logic;
+	Op_Branch:	out std_logic;
+	Op_Mem:		out std_logic;
+	Sig_nUnsig:	out std_logic;
+	Hi_nLo:		out std_logic);
 end;
 
 architecture one of MicroProcessor is
@@ -40,8 +61,25 @@ architecture one of MicroProcessor is
 	port(
 		clk: 			in	std_logic;						--	Clock input signal: Active in RISING Edge
 		reset:			in	std_logic;						--	Reset input signal: Active HIGH
+	
 		Inst_in:		in	std_logic_vector(31 downto 0);	--	Instruction get from the "Intruction Memory"
-		Inst_out:		out	std_logic_vector(31 downto 0));
+		R_Type:			out	std_logic;
+		I_Type:			out	std_logic;
+		J_Type:			out	std_logic;
+		BUS_Type:		out	std_logic;
+	
+		ReadReg1:		out std_logic_vector(4 downto 0);
+		ReadReg2:		out std_logic_vector(4 downto 0);
+		WriteReg:		out std_logic_vector(4 downto 0);
+	
+		Op_Code:		out std_logic_vector(1 downto 0);	-- Output for ALU
+		Op_Shift:		out std_logic;						-- Output for ALU
+		Op_Arith:		out std_logic;						-- Output for ALU
+		Op_Logic:		out std_logic;						-- Output for ALU
+		Op_Branch:		out std_logic;						-- Output for ALU
+		Op_Mem:			out	std_logic;
+		Sig_nUnsig:		out std_logic;						-- Output for ALU	(if the bit '1' the op is with sign bit else the operation unsigned)
+		Hi_nLo:			out std_logic);
 	end component;
 	
 	component MuxProgrammer
@@ -57,13 +95,10 @@ architecture one of MicroProcessor is
 	signal PC_input:						std_logic_vector(31 downto 0);
 	signal PC_output:						std_logic_vector(31 downto 0);
 	signal IR_input:						std_logic_vector(31 downto 0);
-	signal IR_output:						std_logic_vector(31 downto 0);
 	signal Addr_Instruction_Memory_Input:	std_logic_vector(31 downto 0);
 	--/ Signals Declaration \--
 
 begin
-	
-	Instruction_Data_output <= IR_output;
 	
 	U1: PC
 		port map (
@@ -89,8 +124,25 @@ begin
 		port map (
 			clk			=>	clk,
 			reset		=>	reset,
+	
 			Inst_in		=>	IR_input,
-			Inst_out	=>	IR_output);
+			R_Type		=>	R_Type,
+			I_Type		=>	I_Type,
+			J_Type		=>	J_Type,
+			BUS_Type	=>	BUS_Type,
+			
+			ReadReg1	=>	ReadReg1,
+			ReadReg2	=>	ReadReg2,
+			WriteReg	=>	WriteReg,
+	
+			Op_Code		=>	Op_Code,
+			Op_Shift	=>	Op_Shift,
+			Op_Arith	=>	Op_Arith,
+			Op_Logic	=>	Op_Logic,
+			Op_Branch	=>	Op_Branch,
+			Op_Mem		=>	Op_Mem,
+			Sig_nUnsig	=>	Sig_nUnsig,
+			Hi_nLo		=>	Hi_nLo);
 			
 	U5:	MuxProgrammer
 		port map (
