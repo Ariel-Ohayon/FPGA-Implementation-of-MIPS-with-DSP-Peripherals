15d1eda15ba8fc214938f7116fd2ff76a01d9866
Ariel Ohayon <83784945+Ariel-Ohayon@users.noreply.github.com>
2023-03-16 17:58:00 +0200

    Update Forward_Unit.vhd

--- FPGA Design/hdl/Forward_Unit.vhd
+++ FPGA Design/hdl/Forward_Unit.vhd
@@ -1,61 +1,55 @@
--- SubModule: Forward_Unit --
-
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity Forward_Unit is
 port(
+	clk:	in	std_logic;
+	reset:	in	std_logic;
 	Addr_Reg1_STG2:in	std_logic_vector(4 downto 0);
 	Addr_Reg2_STG2:in	std_logic_vector(4 downto 0);
 	Addr_Reg_Wr_STG3:in	std_logic_vector(4 downto 0);
-	Addr_Reg_Wr_STG4:in	std_logic_vector(4 downto 0);
-	selector:out	std_logic_vector(1 downto 0);
-	data_in_STG3:in	std_logic_vector(31 downto 0);
-	data_in_ALU_STG4:in	std_logic_vector(31 downto 0);
-	data_in_MEM_STG4:in	std_logic_vector(31 downto 0);
-	data_out:out	std_logic_vector(31 downto 0);
-	WB_Sel:	in	std_logic;
 	En_IF_ID:	out	std_logic;
 	En_ID_EX:	out	std_logic;
 	En_EX_MEM:	out	std_logic);
 end;
 
 architecture one of Forward_Unit is
+	type state is(s0,s1,s2,s3);
+	signal ps,ns:	state;
 begin
-	process(Addr_Reg1_STG2,Addr_Reg2_STG2,
-			Addr_Reg_Wr_STG3,Addr_Reg_Wr_STG4,
-			data_in_STG3,data_in_ALU_STG4,data_in_MEM_STG4,WB_Sel)begin
-		
-		En_IF_ID  <= '1';
-		En_ID_EX  <= '1';
-		En_EX_MEM <= '1';
-		
-		data_out <= (others => '0');
-		selector <= (others => '0');
-		if(Addr_Reg1_STG2 = "00000" and Addr_Reg2_STG2 = "00000")then
-			En_IF_ID  <= '1';
-			En_ID_EX  <= '1';
-			En_EX_MEM <= '1';
-			data_out <= (others => '0');
-			selector <= (others => '0');
-		elsif(not(Addr_Reg1_STG2 = "00000") and Addr_Reg1_STG2 = Addr_Reg_Wr_STG3)then
-			En_IF_ID <= '0';
-			En_ID_EX <= '1';
-		elsif(not(Addr_Reg2_STG2 = "00000") and Addr_Reg2_STG2 = Addr_Reg_Wr_STG3)then
-			En_IF_ID <= '0';
-			En_ID_EX <= '1';
-		elsif(not(Addr_Reg1_STG2 = "00000") and Addr_Reg1_STG2 = Addr_Reg_Wr_STG4)then
-			En_IF_ID  <= '0';
-			En_ID_EX  <= '1';
-			En_EX_MEM <= '1';
-		elsif(not(Addr_Reg2_STG2 = "00000") and Addr_Reg2_STG2 = Addr_Reg_Wr_STG4)then
-			En_IF_ID  <= '0';
-			En_ID_EX  <= '1';
-			En_EX_MEM <= '1';
-		else
-			En_IF_ID  <= '1';
-			En_ID_EX  <= '1';
-			En_EX_MEM <= '1';
+	process(reset,clk)begin
+		if(reset = '1')then
+			ps <= s0;
+		elsif(clk 'event and clk = '1')then
+			ps <= ns;
 		end if;
+	end process;
+	process(ps,Addr_Reg1_STG2,Addr_Reg2_STG2,Addr_Reg_Wr_STG3)begin
+		case(ps)is
+			when s0 =>
+				En_IF_ID <= '1';
+				En_ID_EX <= '1';
+				En_EX_MEM <= '1';
+				if((Addr_Reg1_STG2 = Addr_Reg_Wr_STG3) or (Addr_Reg2_STG2 = Addr_Reg_Wr_STG3))then
+					ns <= s1;
+				else
+					ns <= s0;
+				end if;
+			when s1 =>
+				En_IF_ID <= '0';
+				En_ID_EX <= '1';
+				En_EX_MEM <= '1';
+				ns <= s2;
+			when s2 =>
+				En_IF_ID <= '0';
+				En_ID_EX <= '0';
+				En_EX_MEM <= '1';
+				ns <= s3;
+			when s3 =>
+				En_IF_ID <= '0';
+				En_ID_EX <= '0';
+				En_EX_MEM <= '0';
+				ns <= s0;
+		end case;
 	end process;
 end;
